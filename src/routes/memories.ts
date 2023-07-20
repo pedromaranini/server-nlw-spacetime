7c6e6a18ba406c32c046bb8bdf17d532002bc7f1
pedromaranini <pedrolucasmaranini30@gmail.com>
2023-07-20 17:09:34 -0300

    initial commit

---
+++ src/routes/memories.ts
@@ -0,0 +1,140 @@
+/* eslint-disable prettier/prettier */
+import { FastifyInstance } from "fastify"
+import { prisma } from "../lib/prisma"
+import { z } from 'zod'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.addHook("preHandler", async (request) => {
+    await request.jwtVerify()
+  })
+
+  // Routes
+  app.get('/memories', async (request) => {
+    const memories = await prisma.memory.findMany({
+      where: {
+        userId: request.user.sub
+      },
+      orderBy: {
+        createdAt: 'asc',
+      }
+    })
+
+    return memories.map(memory => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...')
+      }
+    })
+  })
+
+  app.get('/memories/:id', async (request, reply) => {
+    // Validando se o id existe na requisição (através do zod)
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    // Encontrando a memória onde o id é igual ao id recebido no parâmetro
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      }
+    })
+
+    if (!memory.isPublic && memory.userId !== request.user.sub) {
+      return reply.status(401).send()
+    }
+
+    return memory
+  })
+
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, isPublic, coverUrl } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        isPublic,
+        coverUrl,
+        userId: request.user.sub
+      }
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request, reply) => {
+    // Validando se o id existe na requisição (através do zod)
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    // Validando o body
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, isPublic, coverUrl } = bodySchema.parse(request.body)
+
+    let memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      }
+    })
+
+    if (memory.userId !== request.user.sub) {
+      return reply.status(401).send()
+    }
+
+    memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        isPublic,
+        coverUrl,
+      }
+    })
+
+    return memory;
+  })
+
+  app.delete('/memories/:id', async (request, reply) => {
+    // Validando se o id existe na requisição (através do zod)
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      }
+    })
+
+    if (memory.userId !== request.user.sub) {
+      return reply.status(401).send()
+    }
+
+    // Encontrando a memória onde o id é igual ao id recebido no parâmetro
+    await prisma.memory.delete({
+      where: {
+        id,
+      }
+    })
+  })
+}
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true, // Todas URLs de front-end poderão acessar nosso back-end
+})
+
+app.register(jwt, {
+  secret: 'spacetime', // Token JWT
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
