bf7657fa6dc015fe8022f2c29ae29a116e6c7786
pedromaranini <pedrolucasmaranini30@gmail.com>
2023-07-25 18:38:03 -0300

    upload files

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+app.register(multipart)
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // Todas URLs de front-end poderão acessar nosso back-end
@@ -18,6 +28,7 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
